3063490f0fcd9355f3a9702b0aa30b34d03e05b5
Sam <sambrianbarrett@gmail.com>
2025-02-22 11:36:40 +0000

    scaling the map for correct look

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -22,12 +22,12 @@ int main(int argc, char* argv[])
 	mapRenderer->setModel(core->getResources()->load<Model>("../assets/map/c1a0.obj"));
 
 	map->getComponent<Transform>()->setPosition(glm::vec3(0, 0, 0.0)); 
-	map->getComponent<Transform>()->setScale(glm::vec3(1.0, 1.0, 1.0)); 
+	map->getComponent<Transform>()->setScale(glm::vec3(1.0, 1.0, 1.3));
 
 
 	std::shared_ptr<BoxCollider> mapBoxCollider = map->addComponent<BoxCollider>();
 	mapBoxCollider->setOffset(glm::vec3(0.0, 0.0, 0.0));
-	mapBoxCollider->setDebugRenderer(true);
+	mapBoxCollider->setDebugRenderer(false);
 	mapBoxCollider->setSize(glm::vec3(20.0, 2.0, 20.0));
 	mapBoxCollider->setOffset(glm::vec3(0.0, -13.0, -20.0));
 
@@ -52,7 +52,7 @@ int main(int argc, char* argv[])
 	std::shared_ptr<BoxCollider> playerBoxCollider = player->addComponent<BoxCollider>();
 	playerBoxCollider->setSize(glm::vec3(2.5, 4.0, 2.5));
 	playerBoxCollider->setOffset(glm::vec3(0.0, 1.75, 0.0));
-	playerBoxCollider->setDebugRenderer(true);
+	playerBoxCollider->setDebugRenderer(false);
 
 	std::shared_ptr<RigidBody> playerRigidBody = player->addComponent<RigidBody>();
 	playerRigidBody->setMass(10.0);
@@ -69,7 +69,7 @@ int main(int argc, char* argv[])
 	std::shared_ptr<BoxCollider> entityBoxCollider = entity->addComponent<BoxCollider>();
 	entityBoxCollider->setSize(glm::vec3(1.0, 1.0, 1.0));
 	entityBoxCollider->setOffset(glm::vec3(0.0, 0.5, 0.0));
-	entityBoxCollider->setDebugRenderer(true);
+	entityBoxCollider->setDebugRenderer(false);
 
 	std::shared_ptr<RigidBody> entityRigidBody = entity->addComponent<RigidBody>();
 	entityRigidBody->setMass(10.0);
